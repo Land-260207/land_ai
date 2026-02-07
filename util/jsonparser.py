80ef0c97ec9d0bcc620652c8c7e72821bc7442c0
yeeeengyu <chldlsrb08@naver.com>
2026-02-08 02:12:41 +0900

    chore & fix: 파일병합 & 제미나이 GSG적용

--- util/jsonparser.py
+++ util/jsonparser.py
@@ -5,3 +5,10 @@ with open("util/land.json", "r") as f:
 lands = [row["Column2"] for row in data['data']]
 print(lands)
 print(len(lands))
+
+with open("util/land.json", "r") as f:
+    data = json.load(f)["data"]
+
+land = [row for row in data if isinstance(row.get('※ 매월 말일자 통계 현황'), str) and row['※ 매월 말일자 통계 현황'].isdigit() and len(row['※ 매월 말일자 통계 현황']) == 10 and row['※ 매월 말일자 통계 현황'].endswith('000000') and not row['※ 매월 말일자 통계 현황'].endswith('00000000')]
+lands = [row["Column2"] for row in land]
+print(len(lands))
