463e9d4855821166c0f939f07ff7759d96bd0232
yeeeengyu <chldlsrb08@naver.com>
2026-02-22 22:52:45 +0900

    feature: 사건사고 값변환기 추가

--- util/changevalue.py
+++ util/changevalue.py
@@ -1,12 +1,35 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from util.prompt import getPrompt
-from util.getNews import newsCrawler
+from util.prompt import getPrompt, getAccidentPrompt
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
 
 
 def sync_inflation(region: str, title: str):
     PROMPT = getPrompt(region, title)
+    print(title)
+    messages = [
+        {"role": "system", "content": PROMPT},
+        {"role": "user", "content": title},
+    ]
+    
+    inputs = tokenizer.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(model.device)
+    
+    outputs = model.generate(**inputs, max_new_tokens=6,
+                             do_sample = False)
+    text = tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:], skip_special_tokens=True)
+    
+    import re
+    gen_text = re.search(r"[-+]?\d+(\.\d+)?", text)
+    return gen_text.group() if gen_text else "0.0"
+
+def accident_valuation(region: str, title: str):
+    PROMPT = getAccidentPrompt(region, title)
     print(title)
     messages = [
         {"role": "system", "content": PROMPT},
